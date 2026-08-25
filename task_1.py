582bb6532954e9f56ae00a83be06fbe7dd6daa23
Margarita Vovk <asfalt@192.168.1.6>
2026-08-25 13:16:57 +0200

    Сделать задание 1

---
+++ task_1.py
@@ -0,0 +1,15 @@
+periods = ['1h 45m','360s','25m','30m 120s','2h 60s']
+total_minutes = 0
+
+for period in periods:
+    times = period.split()
+    #print(times)
+    for time in times:
+        if 'h' in time:
+           total_minutes += int(time.replace('h','')) * 60
+        elif 'm' in time:
+            total_minutes += int(time.replace('m',''))
+        elif 's' in time:
+            total_minutes += int(time.replace('s','')) / 60
+
+print(f"Total minutes: {total_minutes}")
