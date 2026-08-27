6b75cad4acc1240c42b58a9ac5fd5cebf3790bbc
Margarita Vovk <asfalt@192.168.1.6>
2026-08-27 14:45:08 +0200

    Выполнить задание 5

--- task_3.py
+++ task_3.py
@@ -16,10 +16,3 @@ for key, value in world_champions.items():
 if 'Италия' in countries:
     print('Италия cтановилась чемпионом мира по футболу в 21 веке!')
 else: print('Италия не выигрывала чемпионат мира по футболу в 21 веке.')
-
-
-
-
-#    for year, country in world_champions.items():
- #       if country == 'Италия':
-  #          print(year)
